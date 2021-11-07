19b453c573a5484981ead7e016e206fb8d95b2a5
mustafa <mustafa@Seralp.localdomain>
2021-11-07 21:30:15 +0300

    Covid Assignment Done

---
+++ Covid Assignment.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+age = input('Are you a cigarette addict older than 75 years old ? yes or on :')
+chronic = input('Do you have a severe chronic disease? yes or no :')
+immune = input('Is your immune system too weak? yes or no :') 
+
+if age  == 'yes' :
+    print ('You are in danger')
+elif chronic == 'yes' :
+    print ('You are in danger')
+elif immune == 'yes' :
+    danger = immune
+    print ('You are in danger')
+  
+    
+else :
+        print ('You are not in risky group') 
+
