b53878a8f3c1af1a3413a5c9c3fd1cc856078fef
mustafa <mustafa@Seralp.localdomain>
2021-11-07 21:52:15 +0300

    Covid Assingment Changed

--- Covid Assignment.py
+++ Covid Assignment.py
@@ -4,19 +4,14 @@
 # In[ ]:
 
 
-age = input('Are you a cigarette addict older than 75 years old ? yes or on :')
-chronic = input('Do you have a severe chronic disease? yes or no :')
-immune = input('Is your immune system too weak? yes or no :') 
+# COVİD SORUSU ASSİNGMENT
+age = input('Are you a cigarette addict older than 75 years old ? yes or on :') == 'yes'
+chronic = input('Do you have a severe chronic disease? yes or no :') == 'yes'
+immune = input('Is your immune system too weak? yes or no :') == 'yes'
 
-if age  == 'yes' :
-    print ('You are in danger')
-elif chronic == 'yes' :
-    print ('You are in danger')
-elif immune == 'yes' :
-    danger = immune
-    print ('You are in danger')
-  
+if age or chronic or immune :
+    print('You are in risky group ')
     
 else :
-        print ('You are not in risky group') 
+    print ('You are not in risky group')
 
