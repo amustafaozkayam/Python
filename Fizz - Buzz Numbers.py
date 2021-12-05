e5022318a133bd57f5912c994dab489aa7ede235
amustafaozkayam <93796510+amustafaozkayam@users.noreply.github.com>
2021-12-05 16:59:25 +0300

    Add files via upload
    Fizz-Buzz numbers added

---
+++ Fizz - Buzz Numbers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+for i in range(1,101):
+    if i % 15 == 0:print(f'({i})FizzBuzz')
+    elif i % 5 == 0:print(f'({i})Buzz')
+    elif i % 3 == 0: print(f'({i})Fizz')
+    else : print('',i)
+
+
+# In[ ]:
+
+
+
+
